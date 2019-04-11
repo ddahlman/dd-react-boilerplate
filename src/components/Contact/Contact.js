28a83fbb4a7b739f9b02b4b18a9f63bd32db6673
therealDaze <danieldahlman@outlook.com>
2019-04-11 17:53:39 +0200

    added Contact

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Conatact.scss';
+
+const Contact = () => {
+	return (
+		<div className={style.container}>
+			<h1>Contact</h1>
+		</div>
+	);
+};
+
+export default Contact;
